0cf218be997b15d1c0d8b7e42b7ce2e3326befcd
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-01-15 14:15:26 +0200

    Add autodetect runing directory

--- wttr-locale.sh
+++ wttr-locale.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-USER=elektronik
-HOME=$(getent passwd $USER | sed -e "s/.*:\/home\///" -e "s/:\/bin\/.*//")
-curl http://wttr.in/Lutsk 2>/dev/null | sed --file /home/$HOME/wttr-locale.txt 1>/dev/tty1
+DIR=`dirname $0`
+curl http://wttr.in/Lutsk 2>/dev/null | sed --file $DIR/wttr-locale.txt 1>/dev/tty1

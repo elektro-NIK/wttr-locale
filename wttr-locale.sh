27d30868588156be73479b5336703cd19886f5bf
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-01-15 14:30:53 +0200

    City as parameter wttr-locale.sh

--- wttr-locale.sh
+++ wttr-locale.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR=`dirname $0`
-curl http://wttr.in/Lutsk 2>/dev/null | sed --file $DIR/wttr-locale.txt 1>/dev/tty1
+curl http://wttr.in/$1 2>/dev/null | sed --file $DIR/wttr-locale.txt 1>/dev/tty1

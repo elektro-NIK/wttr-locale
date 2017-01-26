ecf3e2edee69a2b20163ed0450d3d4deeee318a0
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-01-26 21:48:24 +0200

    Add Last update feature

--- wttr-locale.sh
+++ wttr-locale.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DIR=`dirname $0`
-curl http://wttr.in/$1 2>/dev/null | sed --file $DIR/wttr-locale.txt 1>/dev/tty1
+TIME=`date '+Оновлено: %H:%M'`
+curl http://wttr.in/$1 2>/dev/null | sed --expression="1s/$/\t $TIME/" | sed --file $DIR/wttr-locale.txt 1>/dev/tty1
